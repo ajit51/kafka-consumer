728203eb6bc484575f7defb7f60291faeabe2084
ajit51 <ajitk433@gmail.com>
2024-03-05 19:24:12 +0530

    consuming text message from the kafka topic usinf java configuration file

--- src/main/java/com/kafka/consumer/api/constant/AppConstant.java
+++ src/main/java/com/kafka/consumer/api/constant/AppConstant.java
@@ -2,8 +2,9 @@ package com.kafka.consumer.api.constant;
 
 public class AppConstant {
 
-    public static final String TOPIC_NAME = "javatechie-demo2";
+    //public static final String TOPIC_NAME = "javatechie-demo2";
+    public static final String TOPIC_NAME = "kafka_test_topic2";
     public static final int NUMBER_OF_PARTITION = 3;
     public static final short NUMBER_OF_REPLICATION_FACTOR = 2;
-    public static final String GROUP_ID = "jt-group-1";
+    public static final String GROUP_ID = "jt-group-2";
 }
