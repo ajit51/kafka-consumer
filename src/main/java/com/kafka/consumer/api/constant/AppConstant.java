9f0bc65a446a741e98cbe2779ff2758a3fec0eb3
ajit51 <ajitk433@gmail.com>
2024-03-06 22:45:12 +0530

    consuming object message from the kafka topic usinf java configuration file

--- src/main/java/com/kafka/consumer/api/constant/AppConstant.java
+++ src/main/java/com/kafka/consumer/api/constant/AppConstant.java
@@ -3,8 +3,8 @@ package com.kafka.consumer.api.constant;
 public class AppConstant {
 
     //public static final String TOPIC_NAME = "javatechie-demo2";
-    public static final String TOPIC_NAME = "kafka_test_topic2";
+    public static final String TOPIC_NAME = "kafka_test_topic3";
     public static final int NUMBER_OF_PARTITION = 3;
     public static final short NUMBER_OF_REPLICATION_FACTOR = 2;
-    public static final String GROUP_ID = "jt-group-2";
+    public static final String GROUP_ID = "jt-group-3";
 }
--- src/main/java/com/kafka/consumer/api/consumer/KafkaMessageListener.java
+++ src/main/java/com/kafka/consumer/api/consumer/KafkaMessageListener.java
@@ -1,20 +1,21 @@
 package com.kafka.consumer.api.consumer;
 
 import com.kafka.consumer.api.constant.AppConstant;
+import com.kafka.consumer.api.dto.Customer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import static java.lang.String.format;
-
 @Service
-public class MessageConsumer {
+public class KafkaMessageListener {
 
-    private static final Logger LOGGER = LogManager.getLogger(MessageConsumer.class);
+    private static final Logger LOGGER = LogManager.getLogger(KafkaMessageListener.class);
+
 
     @KafkaListener(topics = AppConstant.TOPIC_NAME, groupId = AppConstant.GROUP_ID)
-    public void consumeMessage(String message) {
-        LOGGER.info(format("Consuming the message from " + AppConstant.TOPIC_NAME + " topic:: %s", message));
+    public void consumeEvents(Customer customer) {
+        LOGGER.info("consumer consume the events {} ", customer.toString());
     }
 }
